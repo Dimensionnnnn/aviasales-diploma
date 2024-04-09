1f366c80a9ade5d429f1d7a62ab91f74c99efe0b
dimensionnn <t.golovin55@gmail.com>
2024-04-09 20:43:21 +0600

    feat: init proj

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,31 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@app': './src/app',
+          '@pages': './src/pages',
+          '@widgets': './src/widgets',
+          '@features': './src/features',
+          '@entities': './src/entities',
+          '@shared': './src/shared',
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+  ],
 };
+
